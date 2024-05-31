1e635f06ee35ce4a7717f2c2eb0874ffa4adfb59
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 14:13:42 +0200

    Add project files.

---
+++ ovning list 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace övning_list._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            Console.WriteLine("Skriv in namn (tryck Enter för att avsluta):");
+
+            string input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                names.Add(input);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nNamn med bokstäverna baklänges:");
+            foreach (var name in names)
+            {
+                char[] charArray = name.ToCharArray();
+                Array.Reverse(charArray);
+                string reversedName = new string(charArray);
+                Console.WriteLine(reversedName);
+            }
+        }
+    }
+}
